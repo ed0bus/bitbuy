21019a5e0ab77515ca92c7ad370c0bd526732a1b
Edoardo Bussani <edoardobussani@gmail.com>
2022-05-24 17:44:57 +0200

    key fix

--- exchange/settings.py
+++ exchange/settings.py
@@ -16,18 +16,18 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-l6h&9*8p5yx*6px3puae8bzzs%vce=2m=_76fa6)hrvj)((u#a'
+
+# SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['arcane-beach-58381.herokuapp.com']
 
 # Application definition
 
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'libraries':{
+            'libraries': {
                 'app_tags': 'app.templatetags.app_tags',
 
             }
@@ -76,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'exchange.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -84,14 +83,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'exchange',
-
-        
+        'CLIENT': {
+            'host': 'mongodb+srv://edo93:canegatto93@cluster0.jystz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'},
+        'authMechanism': 'SCRAM-SHA-1',
 
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -111,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -124,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -140,4 +135,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'signin'
-
