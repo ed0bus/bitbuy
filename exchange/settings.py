4d002ca03bdbd9b1a707ef1f5013f5f02e79536b
Edoardo Bussani <edoardobussani@Edoardos-Mac-mini.local>
2023-02-09 15:05:18 +0100

    change allowed host

--- exchange/settings.py
+++ exchange/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["bitbuy.herokuapp.com"]
+ALLOWED_HOSTS = ["https://bitbuy.onrender.com"]
 
 # Application definition
 
